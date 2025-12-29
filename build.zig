44542f6d47fe1b5b704fc3db34d437f957630e11
Eric Vincent (arch) <eric@efvincent.com>
2025-12-29 10:15:44 -0500

    Add libleanmanifest.a for GMP symbols on Windows

--- build.zig
+++ build.zig
@@ -67,9 +67,11 @@ pub fn build(b: *std.Build) void {
     
     if (target.result.os.tag == .windows) {
         // On Windows, static libraries are in lib/lean/ directory with lib prefix
+        // Need to link manifest for GMP symbols
         const lean_lib = b.pathJoin(&[_][]const u8{ lean_sysroot, "lib", "lean" });
         lib.addObjectFile(.{ .cwd_relative = b.pathJoin(&[_][]const u8{ lean_lib, "libleanrt.a" }) });
         lib.addObjectFile(.{ .cwd_relative = b.pathJoin(&[_][]const u8{ lean_lib, "libleanshared.dll.a" }) });
+        lib.addObjectFile(.{ .cwd_relative = b.pathJoin(&[_][]const u8{ lean_lib, "libleanmanifest.a" }) });
     } else {
         // On Unix, use standard library search
         const lean_lib = b.pathJoin(&[_][]const u8{ lean_sysroot, "lib", "lean" });
@@ -97,9 +99,11 @@ pub fn build(b: *std.Build) void {
     
     if (target.result.os.tag == .windows) {
         // On Windows, static libraries are in lib/lean/ directory with lib prefix
+        // Need to link manifest for GMP symbols
         const lean_lib = b.pathJoin(&[_][]const u8{ lean_sysroot, "lib", "lean" });
         tests.addObjectFile(.{ .cwd_relative = b.pathJoin(&[_][]const u8{ lean_lib, "libleanrt.a" }) });
         tests.addObjectFile(.{ .cwd_relative = b.pathJoin(&[_][]const u8{ lean_lib, "libleanshared.dll.a" }) });
+        tests.addObjectFile(.{ .cwd_relative = b.pathJoin(&[_][]const u8{ lean_lib, "libleanmanifest.a" }) });
     } else {
         // On Unix, use standard library search
         const lean_lib = b.pathJoin(&[_][]const u8{ lean_sysroot, "lib", "lean" });
